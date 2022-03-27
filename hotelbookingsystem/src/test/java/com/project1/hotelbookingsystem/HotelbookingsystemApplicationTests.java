68753b08b9d4bebefc7d674dd91f35a081ab8dd7
kailashsridhar8 <kailashsridhar8@gmail.com>
2022-03-27 19:54:38 +0530

    added spring project

---
+++ hotelbookingsystem/src/test/java/com/project1/hotelbookingsystem/HotelbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.hotelbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
