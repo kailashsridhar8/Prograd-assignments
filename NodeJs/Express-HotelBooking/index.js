2436763e23c11fd4e783fbd68ebed09380469586
kailashsridhar8 <kailashsridhar8@gmail.com>
2022-05-06 01:08:30 +0530

    Formatted

--- NodeJs/Express-HotelBooking/index.js
+++ NodeJs/Express-HotelBooking/index.js
@@ -1,36 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors=require('cors');
-const userRoute= require('./routes/user.route');
-const adminRoute=require('./routes/admin-route');
-const hotelRoute=require('./routes/hotel-route');
+const cors = require("cors");
+const userRoute = require("./routes/user.route");
+const adminRoute = require("./routes/admin-route");
+const hotelRoute = require("./routes/hotel-route");
 
-const secureRoute = require('./routes/secure-routes');
-const bodyParser= require('body-parser');
-const config = require('./config/db.config');
-const passport = require('passport');
+const secureRoute = require("./routes/secure-routes");
+const bodyParser = require("body-parser");
+const config = require("./config/db.config");
+const passport = require("passport");
 
 app.use(cors());
-// app.use('/', userRoute);
 
-// app.use((req,res,next)=>{
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,DELETE")
-//     res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization")
-//     next()
-// })
-
-
-
-
- app.use('/user/auth', passport.authenticate('jwt', { session: false }), secureRoute);
+app.use(
+  "/user/auth",
+  passport.authenticate("jwt", { session: false }),
+  secureRoute
+);
 app.use(bodyParser.json());
 
-app.listen(8000,()=>{
-    console.log('listening on http://localhost:8000');
+app.listen(8000, () => {
+  console.log("listening on http://localhost:8000");
 });
 
- app.use('/user',userRoute);
- app.use('/admin',adminRoute);
- app.use('/hotel',hotelRoute);
-
+app.use("/user", userRoute);
+app.use("/admin", adminRoute);
+app.use("/hotel", hotelRoute);
